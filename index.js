6f39401acdad98c651f304da5faefb3b2c6e2390
Ashok Goli <goliashok645@gmail.com>
2023-09-08 20:20:16 +0530

    title updated

--- index.js
+++ index.js
@@ -7,37 +7,30 @@ function search() {
     Status.innerHTML = "Loading..."
     var value = inputElement.value
     console.log(value)
-    $.get("https://www.omdbapi.com/?apikey=45f0782a&s="+value,function(response){
+    $.get("https://www.omdbapi.com/?apikey=45f0782a&s=" + value, function (response) {
         Status.innerHTML = ""
-    if(inputElement.value==""){
-        alert("Enter movie name!!")
-    }
-    if(response.Response == "True"){
-        var movieData = response
-        for(var i=0;i<movieData.Search.length;i++){
-            movieWrapper.innerHTML += `
+        if (inputElement.value == "") {
+            alert("Enter movie name!!")
+        }
+        if (response.Response == "True") {
+            var movieData = response
+            for (var i = 0; i < movieData.Search.length; i++) {
+                movieWrapper.innerHTML += `
             <div id="movie-card">
                         <img
                             src=${movieData.Search[i].Poster} />
                         <div id="data">
-                            <table>
-                                <tr>
-                                    <th>Title :</th>
-                                    <td>${movieData.Search[i].Title}</td>
-                                </tr>
-                                <tr>
-                                    <th>Year :</th>
-                                    <td>${movieData.Search[i].Year}</td>
-                                </tr>
-                            </table>
+                        <p><b>Title: </b><span>${movieData.Search[i].Title}</span> </p>
+                <p><b>Year: </b><span>${movieData.Search[i].Year}</span> </p>
+                            
                         </div>
                     </div>`
+            }
+        } else {
+            Status.innerText = "---404 Error Movie Not found---"
         }
-    }else{
-        Status.innerText = "---404 Error Movie Not found---"
-    }
 
 
-})
+    })
 }
 
